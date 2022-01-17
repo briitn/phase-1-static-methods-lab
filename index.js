3f00593f2de2e3827112e0605aaebc4097d529a5
princegobah <babynamek@gmail.com>
2022-01-17 17:40:44 -0500

    commit tree 998471997fb3c01b937f0356af32dd1f5a61851b

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+static capitalize(string){
+  return string.charAt(0).toUpperCase()+string.slice(1)
 }
+static sanitize(string){
+  return string.replace(/[^A-Za-z0-9-' " "]+/g,'');
+  
+}
+
+static titleize(string){
+  string = string[0].toUpperCase() + string.slice(1);
+  let splitString = string.split(" ");
+  let dontCap = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+  for(let i = 0; i < splitString.length; i++){
+    if(!(dontCap.includes(splitString[i]))){
+      splitString[i] = splitString[i][0].toUpperCase() + splitString[i].slice(1);
+    }
+  }
+  return splitString.join(" ");
+}
+  
+}
+
